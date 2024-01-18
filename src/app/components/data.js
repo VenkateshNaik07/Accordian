62382d414226e66e65b72c8abdf2152c76d2e877
Venkatesh Naik <123453183+VenkateshNaik07@users.noreply.github.com>
2024-01-18 19:59:47 +0530

    Accordian src added
    This multi selction accordian react project

---
+++ src/app/components/data.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+      "id": 1,
+      "question": "What is the capital of France?",
+      "answer": "Paris"
+    },
+    {
+      "id": 2,
+      "question": "Who wrote 'Romeo and Juliet'?",
+      "answer": "William Shakespeare"
+    },
+    {
+      "id": 3,
+      "question": "What is the largest planet in our solar system?",
+      "answer": "Jupiter"
+    },
+    {
+      "id": 4,
+      "question": "In which year did World War II end?",
+      "answer": "1945"
+    },
+    {
+      "id": 5,
+      "question": "Who is known as the 'Father of Computers'?",
+      "answer": "Charles Babbage"
+    }
+  ];
+
+export default data;
+  
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import Accordian from './components'
+
+export default function Home() {
+  return (
+    <main>
+      <Accordian />
+    </main>
+  )
+}
